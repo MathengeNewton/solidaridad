a72fe7ba1afc09c310c981ea60fa4ee9c4f895b0
MathengeNewton <rexynewton79@gmail.com>
2024-03-09 23:59:21 +0300

    Initial configurations for tailwind and tsconfig

---
+++ src/types/cardTypes.tsx
@@ -0,0 +1,12 @@
+export interface HeroSectionCardsPropsType {
+    backgroundImage: string;
+    title: string;
+    buttonText: string;
+};
+
+export interface ProductProps{
+    imageSrc: string;
+    productName: string;
+    productPrice: string;
+}
+  
